0082532504c96f5c498d5437e92aee9c8c4a07ee
Unknown <njs03332@gmail.com>
2018-07-16 13:41:46 -0600

    controller pass eliminated

--- app/controllers/soom_cards_controller.rb
+++ app/controllers/soom_cards_controller.rb
@@ -1,36 +1,28 @@
 class SoomCardsController < ApplicationController
     def index
-        pass
     end
 
     def new
-        pass
     end
 
 
     def create
-        pass
     end
 
     def destroy
-        pass
     end
 
 
     def day_list
-        pass
     end
 
     def deul_soom
-        pass
     end
 
     def nal_soom
-        pass
     end
 
     def notice
-    
     end
 
 end
