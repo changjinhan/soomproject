9dbe964c22b3d925cf081dc4537682dc605a5c86
Unknown <njs03332@gmail.com>
2018-07-16 20:26:25 -0600

    add columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180707130731) do
+ActiveRecord::Schema.define(version: 20180716170349) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180707130731) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -45,6 +46,10 @@ ActiveRecord::Schema.define(version: 20180707130731) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "fb_name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
