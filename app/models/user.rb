5666c9ce2e9526b41d7d0edf24094bb83a28612f
Unknown <njs03332@gmail.com>
2018-07-27 14:31:25 -0600

    added code for debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,17 +16,22 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.find_for_facebook_oauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    puts "----------------------1111111111111"
+    puts auth.info
+    puts auth.info.email
+    puts auth.info.name
+    where(email: auth.info.email).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
+      # user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      user.save
     end
     # 이 때는 이상하게도 after_create 콜백이 호출되지 않아서 아래와 같은 조치를 했다.
     # user.add_role :user if user.roles.empty?
-    user # 최종 반환값은 user 객체이어야 한다.
+    # 최종 반환값은 user 객체이어야 한다.
   end
   
   def self.new_with_session(params, session)
@@ -36,6 +41,14 @@ class User < ApplicationRecord
       end
     end
   end
+
+  # def after_sign_in_path_for
+  #   auth = request.env['omniauth.auth']
+  #   @user = User.find(current_user.id)
+  #   if @user.persisted?
+  #     soom_card_path
+  #   else
+      
 
 
  end
