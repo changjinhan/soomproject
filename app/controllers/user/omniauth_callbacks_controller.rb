ab4cb87b512dd22bf260a5ebf3c4e0be447330dc
Unknown <njs03332@gmail.com>
2018-07-27 14:32:07 -0600

    omniauth callbacks controller modified

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -1,8 +1,19 @@
 class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
+      puts "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRs"
+      puts request.env["omniauth.auth"]
+      puts "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
   
+      puts "---------------------------------"
+      puts @user
+      puts @user.provider
+      puts @user.uid
+      puts @user.email
+      puts @user.name
+      puts @user.password
+      puts @user.image
       if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
@@ -10,5 +21,9 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         session["devise.facebook_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
       end
+    end
+
+    def failure
+      redirect_to root_path
     end
   end   
