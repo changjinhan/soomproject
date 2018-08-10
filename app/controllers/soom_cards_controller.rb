5885e384dce7644e481258e4094380798e93bffe
Unknown <njs03332@gmail.com>
2018-08-10 13:45:42 -0600

    index controller brings friends using koala

--- app/controllers/soom_cards_controller.rb
+++ app/controllers/soom_cards_controller.rb
@@ -1,5 +1,16 @@
 class SoomCardsController < ApplicationController
+    # before_action :authenticate_user!
+
     def index
+        if user_signed_in?
+            graph = Koala::Facebook::API.new(current_user.token)
+            user = graph.get_object("me")
+            @friends = graph.get_connections(user["id"], "friends")
+            puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            puts @friends
+            
+        end
+        render 'index'
     end
 
     def new
